b14b2b802a080a0d8d75906dbf2eacfba8efec09
FedericoHerreraa <fede.juan.herrera@gmail.com>
2024-11-07 17:04:53 -0300

    updating firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import QRCode from "react-qr-code";
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import HomeUser from "./pages/User/HomeUser";
+import HomeSysAdmin from "./pages/Admin/HomeSysAdmin";
 import HomeAdmin from "./pages/Admin/HomeAdmin";
 import AuthenticationPage from "./components/AuthenticationPage";
-
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/auth" element={<AuthenticationPage />}/>
         <Route path="/home-user" element={<HomeUser />} />
+        <Route path="/home-sys-admin" element={<HomeSysAdmin />} />
         <Route path="/home-admin" element={<HomeAdmin />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Admin/HomeSysAdmin.jsx
@@ -0,0 +1,9 @@
+
+
+export default function HomeSysAdmin() {
+    return (
+        <div>
+            <h1>Esta es la pagina de inicio del administrador</h1>
+        </div>
+    )
+}
