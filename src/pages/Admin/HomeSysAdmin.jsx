48819cc2cb327783b1c1ec41aedea5e4d687c589
FedericoHerreraa <fede.juan.herrera@gmail.com>
2024-11-15 13:19:07 -0300

    updating sys admin page

--- src/pages/Admin/HomeSysAdmin.jsx
+++ src/pages/Admin/HomeSysAdmin.jsx
@@ -3,7 +3,7 @@
 export default function HomeSysAdmin() {
     return (
         <div>
-            <h1>Esta es la pagina de inicio del administrador</h1>
+            <h1>Esta es la pagina de inicio del sys administrador</h1>
         </div>
     )
 }
